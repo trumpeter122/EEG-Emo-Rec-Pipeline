2bb7f61c664909681a728f481728ec02b0c331e2
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2025-12-01 13:39:00 +0800

    feat(scripts): improve tree_src.sh

--- scripts/tree_src.sh
+++ scripts/tree_src.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i zsh -p eza tokei
 
-tree src -I "__pycache__" -C --dirsfirst --sort=name
+# tree src -I "__pycache__" -C --dirsfirst --sort=name
+#
+# echo Total lines:
+# echo $(find src -type d -name __pycache__ -prune -o -type f -name '*.py' -print | xargs cat | wc -l)
 
-echo Total lines:
-echo $(find src -type d -name __pycache__ -prune -o -type f -name '*.py' -print | xargs cat | wc -l)
+eza . --tree --git-ignore --group-directories-first
+tokei .
