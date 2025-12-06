cb887d1109cdb923021eb12779901e6dc879d90e
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2025-12-06 20:34:22 +0800

    feat(replication): replicate research 05

--- src/main.py
+++ src/main.py
@@ -143,4 +143,21 @@ def run_research_paper_04() -> None:
     )
 
 
-run_research_paper_04()
+def run_research_paper_05() -> None:
+    """Replicate Research Paper 05 configuration."""
+    run_pipeline(
+        preprocessing_options=PREPROCESSING_OPTIONS.get_names(["clean"]),
+        channel_pick_options=CHANNEL_PICK_OPTIONS.get_names(["standard_32"]),
+        feature_options=FEATURE_OPTIONS.get_names(["psd"]),
+        segmentation_options=SEGMENTATION_OPTIONS.get_names(["w2.00_s0.25"]),
+        model_options=MODEL_OPTIONS.get_names(["svc_rbf_sklearn"]),
+        build_dataset_options=BUILD_DATASET_OPTIONS.get_names(
+            ["valence+use1.00+test0.30+seed42+classification+standard"]
+        ),
+        training_method_options=TRAINING_METHOD_OPTIONS.get_names(
+            ["sklearn_default_classification"]
+        ),
+    )
+
+
+run_research_paper_05()
